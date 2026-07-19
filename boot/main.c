fed7e2678cbbdcd4c8edef21bcbb124259a79a0e
Ren-deRing <heebb0912.b@gmail.com>
2026-07-19 20:32:44 +0900

    [boot] bitmap font

--- boot/main.c
+++ boot/main.c
@@ -6,6 +6,34 @@
 #define NANOPRINTF_IMPLEMENTATION
 #include "nanoprintf.h"
 
+#include "font.h"
+
+typedef struct {
+    uint32_t magic;  // 0x36, 0x04, 0x03, 0x10
+    uint8_t  mode;
+    uint8_t  char_height;
+} __attribute__((packed)) psf_header_t;
+
+volatile uint32_t *fb;
+uint16_t screen_pitch;
+
+void putc(char c, int x, int y, uint32_t fg) { // prototype. will be fixed.
+    uintptr_t font_addr = (0xFFFF800000000000ULL + (uintptr_t)font);
+    
+    psf_header_t *header = (psf_header_t *)font_addr;
+    uint8_t *glyph = (uint8_t *)font_addr + 4 + (c * 16);
+
+    for (int cy = 0; cy < 16; cy++) {
+        uint8_t line = glyph[cy];
+        for (int cx = 0; cx < 8; cx++) {
+            if (line & (0x80 >> cx)) {
+                uint32_t fb_index = (y + cy) * (screen_pitch / 4) + (x + cx);
+                fb[fb_index] = fg;
+            }
+        }
+    }
+}
+
 typedef struct {
     uint64_t base_addr;     // memory addr
     uint64_t length;        // memory area length
@@ -36,19 +64,24 @@ void loader_entry() {
 
     uint16_t screen_width  = (int)*(uint16_t *)(vbe_base + 18);
     uint16_t screen_height = (int)*(uint16_t *)(vbe_base + 20);
-    uint16_t screen_pitch    = *(uint16_t *)(vbe_base + 16);
+    screen_pitch = *(uint16_t *)(vbe_base + 16);
 
     uint64_t vbe_lfb_end = (uint64_t)vbe_lfb_addr + ((uint64_t)screen_pitch * screen_height);
 
     extern void init_hhdm(uint64_t mem_size, uint64_t vbe_lfb_end);
     init_hhdm(get_total_ram(), vbe_lfb_end);
 
-    volatile uint32_t *fb = (volatile uint32_t *)(0xFFFF800000000000ULL + vbe_lfb_addr);
+    fb = (volatile uint32_t *)(0xFFFF800000000000ULL + vbe_lfb_addr);
     uint32_t total_pixels  = (uint32_t)screen_width * screen_height;
 
-    for (uint32_t i = 0; i < total_pixels; i++) {
-        fb[i] = 0x00A8FF; 
-    }
+    putc('k', 100, 100, 0xFFFFFFFF);
+    putc('i', 110, 100, 0xFFFFFFFF);
+    putc('n', 120, 100, 0xFFFFFFFF);
+    putc('i', 130, 100, 0xFFFFFFFF);
+    putc('t', 140, 100, 0xFFFFFFFF);
+    putc('1', 150, 100, 0xFFFFFFFF);
+    putc('4', 160, 100, 0xFFFFFFFF);
+    putc('8', 170, 100, 0xFFFFFFFF);
 
     for (;;) asm __volatile__ ("hlt");
 }
